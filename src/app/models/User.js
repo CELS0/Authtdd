cc46f9326f5ff19b4be2c66c6530ca21ca133998
Celso Jr <cesojr.300@gmail.com>
2021-08-10 21:39:22 -0300

    wip: model user on router

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,9 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password_hash: DataTypes.STRING,
-    });
-
-    return User;
-}
+    const User = sequelize.define(
+        "User",
+        {
+            name: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password_hash: DataTypes.STRING
+        }
+    );
+    return User
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
-const { User } = require('./app/models');
+const User = require('./app/models/User');
+ 
 
-User.create({name: 'Celso', email: 'celso@gmail.com', password_hash: '123456'});
+User.create({ name: 'Celso', email: 'celso@gmail.com', password_hash: '123456' });
 
 module.exports = routes;
