e072bb5dd7d59579422c3eda6cdf0d1d9c59a562
Celso Jr <cesojr.300@gmail.com>
2021-08-12 19:08:55 -0300

    wip: add jest

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define(
-        "User",
+        "users",
         {
             name: DataTypes.STRING,
             email: DataTypes.STRING,
--- src/database/migrations/20210809201734-create-users.js
+++ src/database/migrations/20210809201734-create-users.js
@@ -22,11 +22,11 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      created_at: {
+      createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      updated_at: {
+      updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
       }
